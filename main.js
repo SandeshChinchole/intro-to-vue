ce1fc0e40d37ce8b9dca86a8dabbb11253f25504
Sandesh <31442991+SandeshChinchole@users.noreply.github.com>
2022-08-30 13:53:40 -0400

    completed lesson 2

--- main.js
+++ main.js
@@ -2,9 +2,7 @@ const app = new Vue({
   el: '#app',
   data: {
     product: 'Socks',
-    description: 'A pair of warm, fuzzy socks.',
-    inventory: 'Not available',
-    color: 'red',
-    shipping: 'no',
+    image: './assets/vmSocks-green-onWhite.jpeg',
+    url: 'https://www.github.com',
   },
 });
