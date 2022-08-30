b56cec805ef10a8d24e5aa149a9039af19ab1e3f
Sandesh <31442991+SandeshChinchole@users.noreply.github.com>
2022-08-30 16:39:27 -0400

    completed lesson 4

--- main.js
+++ main.js
@@ -4,7 +4,17 @@ const app = new Vue({
     product: "Socks",
     image: "./assets/vmSocks-green-onWhite.jpeg",
     inStock: true,
-    inventory: 11,
-    onSale: true,
+    details: ["80% cotton", "20% polyester", "Gender-neutral"],
+    sizes: ["S", "M", "L", "XL", "XXL", "XXXL"],
+    variants: [
+      {
+        variantId: 2234,
+        variantColor: "green",
+      },
+      {
+        variantId: 2235,
+        variantColor: "blue",
+      },
+    ],
   },
 });
