4b18695ce675ec1ae48b2170743944e84b682f27
Sandesh <31442991+SandeshChinchole@users.noreply.github.com>
2022-08-30 17:28:48 -0400

    completed lesson 5

--- main.js
+++ main.js
@@ -5,16 +5,29 @@ const app = new Vue({
     image: "./assets/vmSocks-green-onWhite.jpeg",
     inStock: true,
     details: ["80% cotton", "20% polyester", "Gender-neutral"],
-    sizes: ["S", "M", "L", "XL", "XXL", "XXXL"],
     variants: [
       {
         variantId: 2234,
         variantColor: "green",
+        variantImage: "./assets/vmSocks-green-onWhite.jpeg",
       },
       {
         variantId: 2235,
         variantColor: "blue",
+        variantImage: "./assets/vmSocks-blue-onWhite.jpeg",
       },
     ],
+    cart: 0,
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1;
+    },
+    updateProduct(variantImage) {
+      this.image = variantImage;
+    },
+    removeFromCart() {
+      this.cart -= 1;
+    },
   },
 });
