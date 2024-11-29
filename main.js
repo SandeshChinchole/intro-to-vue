30b14127cd012d77cb53dd7d0f6d632697a4f868
Sandesh <31442991+SandeshChinchole@users.noreply.github.com>
2024-11-29 14:14:28 -0500

    refactored

--- main.js
+++ main.js
@@ -4,7 +4,13 @@ const app = new Vue({
     product: "Socks",
     image: "./assets/vmSocks-green-onWhite.jpeg",
     inStock: true,
-    details: ["80% cotton", "20% polyester", "Soft", "Affordable"],
+    details: [
+      "80% cotton",
+      "20% polyester",
+      "Soft",
+      "Affordable",
+      "High Quality",
+    ],
     variants: [
       {
         variantId: 2234,
