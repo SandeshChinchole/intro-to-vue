f1add72c20887e6a72e2f8eff4307a3164c54ef9
Sandesh <31442991+SandeshChinchole@users.noreply.github.com>
2026-01-03 18:17:15 -0500

    refactor: remove options

--- main.js
+++ main.js
@@ -10,20 +10,11 @@ const app = new Vue({
         variantId: 2234,
         variantColor: "green",
         variantImage: "./assets/vmSocks-green-onWhite.jpeg",
-        options: [
-          { size: "Small", cost: "$10" },
-          { size: "Medium", cost: "$20" },
-          { size: "Large", cost: "$30" },
-        ],
       },
       {
         variantId: 2235,
         variantColor: "blue",
         variantImage: "./assets/vmSocks-blue-onWhite.jpeg",
-        options: [
-          { size: "Small", cost: "$10" },
-          { size: "Medium", cost: "$20" },
-        ],
       },
     ],
     cart: 0,
